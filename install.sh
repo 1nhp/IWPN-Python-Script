219d6b9c1e36b7ae68c4726af0a5164ad54c17fb
1nhp <bnbn200923@gmail.com>
2025-04-24 00:47:46 +0300

    asdasdsad

---
+++ install.sh
@@ -0,0 +1,8 @@
+#bin/sh
+
+cp -r data ~/.local/bin/iwpn/
+cp -r config.ini ~/.local/bin/iwpn
+cp -r scripts ~/.local/bin/iwpn
+cp -r init.py ~/.local/bin/iwpn
+mkdir ~/.local/bin/iwpn/data
+mv ~/.local/bin/iwpn/sound_themes ~/.local/bin/iwpn/data
